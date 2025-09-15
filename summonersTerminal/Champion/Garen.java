9382788fb8a2492cd6282cddaf5496014ce82dba
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-09-15 15:06:00 +0200

    Reapply "Push stuff"
    This reverts commit ad74ff708e0eb84a5578ec03d3de76f6884a92b2.

---
+++ summonersTerminal/Champion/Garen.java
@@ -0,0 +1,12 @@
+package summonersTerminal.Champion;
+
+import summonersTerminal.Champion;
+import summonersTerminal.ChampionClass;
+
+public class Garen extends Champion
+{
+    public Garen()
+    {
+        super("Garen", ChampionClass.BRAWLER);
+    }
+}
