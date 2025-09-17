4f89d3096ddcecaad79f888af1be24b6e1a4a967
aletheos <aletheos@outlookcom>
2025-09-17 17:02:51 +0200

    Add additional methods for adding text to the buffer

--- summonersTerminal/gameHelpers/Screen.java
+++ summonersTerminal/gameHelpers/Screen.java
@@ -1,7 +1,4 @@
 package summonersTerminal.gameHelpers;
-
-import java.util.Scanner;
-
 /**
  * The Screen class is responsible for creating a text buffer of fixed size<br>
  * that is drawn to the screen once after every game action.
@@ -9,19 +6,77 @@ import java.util.Scanner;
 public final class Screen {
     // Singleton instance
     private static Screen _screen;
-    private String[] buffer;
 
+    private StringBuilder screenBuffer;
+    // HADO-CODE-O, BAYBEE!
+    private final int screenWidth = 80, screenHeight = 20;
     private Screen() {
-        buffer = new String[20];
+        screenBuffer = new StringBuilder(screenWidth * screenHeight);
+        for (int i = 0; i < screenBuffer.capacity(); i++) {
+            screenBuffer.append(' ');
+        }
     }
 
-    public Screen getInstance() {
+    public static Screen get() {
         if (_screen == null) {
             _screen = new Screen();
         }
         return _screen;
     }
 
+    /**
+     * Will place the formatted text into the screen buffer at the desired position
+     * discarding any text that falls outside of the screen buffer.
+     * @param formattedText
+     */
+    private void addToBuffer(int column, int row, String formattedText) {
+        int startIndex = row * screenWidth + column;
+
+        for (int i = 0; i < formattedText.length(); i++) {
+            char c = formattedText.charAt(i);
+            screenBuffer.setCharAt(startIndex + i, c);
+        }
+    }
+
+    /**
+     * Adds a message into the status window
+     * @param message
+     */
+    public Screen addStatus(String message) {
+        addToBuffer(5, 19, message.substring(0, 63).strip());
+        return this;
+    }
+
+    /**
+     * Add the borders of the statuswindow near the bottom of the buffer
+     */
+    public Screen addStatusWindow() {
+        addToBuffer(
+            3, 18,
+            """
+            ▄■▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀■■▀▄▄
+            █                                                                 ■■■■■██▄▄
+            ▀■▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄■■▄▀▀
+            """
+        );
+        return this;
+    }
+
+    public Screen addIntroMessage() {
+        addToBuffer(0, 0, Copy.initialCopy());
+        return this;
+    }
+
     public void draw() {
+        //Clears the screen if running in a terminal
+        System.out.print("\033[H\033[2J");
+
+        int rowStart = 0;
+        for (int i = 0; i < screenBuffer.length(); i++) {
+            if(i % screenWidth == screenWidth - 1) {
+                System.out.println(screenBuffer.substring(rowStart, i));
+                rowStart = i + 1;
+            }
+        }
     }
 }
